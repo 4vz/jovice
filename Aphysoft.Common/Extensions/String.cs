b5af0ac3063c8b4449d9e3f0ed5c2b3e47523942
afisd <afis.net@gmail.com>
2016-10-09 23:56:12 +0700

    Added string trim extension methods for exceptions

--- Aphysoft.Common/Extensions/String.cs
+++ Aphysoft.Common/Extensions/String.cs
@@ -26,5 +26,51 @@ namespace Aphysoft.Common
             }
             return rv;
         }
+
+        public static bool StartsWith(this string value, params string[] values)
+        {
+            bool ret = false;
+            foreach (string v in values)
+            {
+                if (value.StartsWith(v)) { ret = true; break; }
+            }
+            return ret;
+        }
+
+        private static char[] TrimCharacters(char[] except)
+        {
+            List<char> trimChars = new List<char>();
+            for (int i = 0; i < 255; i++) trimChars.Add((char)i);
+            foreach (char c in except) trimChars.Remove(c);
+
+            return trimChars.ToArray();
+        }
+
+        /// <summary>
+        /// Removes all leading occurrences except of a set of characters especified in an array from the current System.String object.
+        /// </summary>
+        public static string TrimStartExcept(this string value, params char[] trimExceptChars)
+        {
+            if (trimExceptChars == null) return value.TrimStart(null);
+            return value.TrimStart(TrimCharacters(trimExceptChars));
+        }
+
+        /// <summary>
+        /// Removes all trailing occurrences except of a set of characters specified in an array from the current System.String object.
+        /// </summary>
+        public static string TrimEndExcept(this string value, params char[] trimExceptChars)
+        {
+            if (trimExceptChars == null) return value.TrimEnd(null);
+            return value.TrimEnd(TrimCharacters(trimExceptChars));
+        }
+
+        /// <summary>
+        /// Removes all leading and trailing occurrences of a set of characters specified in an array from the current System.String object.
+        /// </summary>
+        public static string TrimExcept(this string value, params char[] trimExceptChars)
+        {
+            if (trimExceptChars == null) return value.Trim(null);
+            return value.Trim(TrimCharacters(trimExceptChars));
+        }
     }
 }

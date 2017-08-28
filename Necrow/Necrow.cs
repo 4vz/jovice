a4b9828d127590c484b6cef906feb4e4e954a349
afisd <afis.net@gmail.com>
2017-08-29 06:52:50 +0700

    Increase Version to 25

--- Necrow/Necrow.cs
+++ Necrow/Necrow.cs
@@ -133,7 +133,7 @@ namespace Center
     {
         #region Fields
 
-        internal readonly static int Version = 24;
+        internal readonly static int Version = 25;
 
         private static Database j = null;
 

039ff117990824598fdc5e9af419a2e1e2b44d43
afisd <afis.net@gmail.com>
2017-11-13 22:47:27 +0700

    Added IsPrivate method to determine whether the IPAddress instance is in private space or not

--- Aphysoft.Share/Extensions/IPAddress.cs
+++ Aphysoft.Share/Extensions/IPAddress.cs
@@ -47,5 +47,26 @@ namespace Aphysoft.Share
 
             return network1.Equals(network2);
         }
+
+        /// <summary>
+        /// An extension method to determine if an IP address is internal, as specified in RFC1918
+        /// </summary>
+        /// <param name="ipAddress">The IP address that will be tested</param>
+        /// <returns>Returns true if the IP is internal, false if it is external</returns>
+        public static bool IsPrivate(this IPAddress ipAddress)
+        {
+            byte[] bytes = ipAddress.GetAddressBytes();
+            switch (bytes[0])
+            {
+                case 10:
+                    return true;
+                case 172:
+                    return bytes[1] < 32 && bytes[1] >= 16;
+                case 192:
+                    return bytes[1] == 168;
+                default:
+                    return false;
+            }
+        }
     }
 }

cd9fd9794aee5fca1b04a15cb2d15a4dd2cc05f7
afisd <afis.net@gmail.com>
2017-11-25 22:54:33 +0700

    Added app configs and manifest to ServiceConsole;

--- ServiceConsole/Program.cs
+++ ServiceConsole/Program.cs
@@ -11,7 +11,7 @@ namespace ServiceConsole
     class Program
     {
         static void Main(string[] args)
-        {
+        {        
             Service.Server(ServiceTraceLevels.Default);
             Server.Init();
         }

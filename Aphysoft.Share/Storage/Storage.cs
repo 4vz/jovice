d3d8fb0add8959472907af57c6504f6b1b9bfdc0
4vz <afis.net@gmail.com>
2018-02-25 06:26:48 +0700

    Changed storage class name to IO

--- Aphysoft.Share/Storage/Storage.cs
+++ Aphysoft.Share/Storage/Storage.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aphysoft.Share
 { 
-    public static class Storage
+    public static class IO
     {
         public static bool HasWriteAccessToDirectory(string folderPath)
         {
             try
             {
-                // Attempt to get a list of security permissions from the folder. 
-                // This will raise an exception if the path is read only or do not have access to view the permissions. 
+                // TODO
+
                 System.Security.AccessControl.DirectorySecurity ds = Directory.GetAccessControl(folderPath);
                 return true;
+
             }
             catch (UnauthorizedAccessException)
             {

e9ac6b0a81d8a9bddd3bf4cd88461c4060032e73
4vz <afis.net@gmail.com>
2018-03-29 18:58:24 +0700

    Added test projects

---
+++ Test/Test.cs
@@ -0,0 +1,53 @@
+﻿using Aphysoft.Share;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aphysoft.Test
+{
+    public class Test : Node
+    {
+        #region Constructors
+
+        public Test() : base("TEST")
+        {
+
+        }
+
+        #endregion
+
+        protected override void OnEvent(string message)
+        {
+            if (IsConsole)
+                Console.WriteLine(message);
+        }
+
+        protected override void OnStart()
+        {
+            Event("Test");
+
+            BeginEdge(IPAddress.Loopback);
+
+            while (IsRunning)
+            {
+                if (!IsRunning)
+                {
+                    Event("Byte");
+                    break;
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            Console.WriteLine("Stop is called");
+
+
+        }
+
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using Aphysoft.Share;
+using System.Net;
+using Aphysoft.Test;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.Start(true);
+        }
+    }
+
+
+}
